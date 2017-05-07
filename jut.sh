1d8b82eeb3c3aed0819dcda1fea62351f4b7f516
stefanSchuhbaeck <stefan@schuhbaeck.eu>
2017-05-07 14:18:36 +0200

    Start work WarningsPluginDocker

--- jut.sh
+++ jut.sh
@@ -1,2 +1,1 @@
-env JENKINS_WAR=/usr/lib/jenkins/jenkins.war PLUGINS_DIR=/var/lib/jenkins/plugins/ WORSPACE=/tmp BROWSER=chrome ./jut-server.sh -n 2
-
+env TYPE=winstone  JENKINS_WAR=/usr/lib/jenkins/jenkins.war PLUGINS_DIR=/var/lib/jenkins/plugins/ WORSPACE=/tmp BROWSER=firefox ./jut-server.sh -n 2 
